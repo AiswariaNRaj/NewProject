271320963d8d01cef6db0fa4135b70d21ffd3706
AiswariaNRaj <aiswarianraj1996@gmail.com>
2021-11-11 19:09:09 +0530

    Initial Commit

--- app/src/main/java/com/example/newproject/Settings_Fragment.java
+++ app/src/main/java/com/example/newproject/Settings_Fragment.java
@@ -1,5 +1,6 @@
 package com.example.newproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,7 @@ public class Settings_Fragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Object LinearLayout;
 
     public Settings_Fragment() {
         // Required empty public constructor
@@ -59,6 +62,17 @@ public class Settings_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings_, container, false);
+        View v = inflater.inflate(R.layout.fragment_settings_, container, false);
+        LinearLayout linearLayout_equalizer = (LinearLayout) v.findViewById(R.id.fragment_settings_ll_equalizer);
+        linearLayout_equalizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), Activity_EqualizerSettings.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
+
     }
 }
