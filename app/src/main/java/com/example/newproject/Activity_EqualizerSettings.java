22d21821053af1039ca7e0e0e6ec30b6a3a57043
AiswariaNRaj <aiswarianraj1996@gmail.com>
2021-11-15 12:12:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/newproject/Activity_EqualizerSettings.java
@@ -0,0 +1,14 @@
+package com.example.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_EqualizerSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equalizer_settings);
+    }
+}
